80c50fdfb840c396c5c761eb611844ec6e9f4a08
valdadyne <rodgers.gitau@gmail.com>
2018-05-28 17:25:07 +0300

    Pixabay-finder:- Get free premium images from Pixabay

---
+++ pixabay-finder/src/components/image-results/ImageResult.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ImageResult extends Component {
+  render() {
+    return <div className="container" />;
+  }
+}
+
+export default ImageResult;
---
+++ pixabay-finder/src/components/search/Search.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import TextField from "material-ui/TextField";
+import SelectField from "material-ui/SelectField";
+import MenuItem from "material-ui/MenuItem";
+import axios from "axios";
+
+class Search extends Component {
+  state = {
+    searchText: "",
+    amount: 15,
+    apiUrl: "https//pixabay.com/api",
+    apiKey: "8810823-b9bdb60f7a53f59642c58678d",
+    images: []
+  };
+  onTextChange = e => {
+    this.setState({ [e.target.name]: e.target.value }, () => {
+      axios
+        .get(
+          `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+            this.state.searchText
+          }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+        )
+        .then(res => this.setState({ images: res.data.hits }))
+        .catch(err => console.log(err));
+    });
+  };
+
+  render() {
+    console.log(this.state.images);
+    return (
+      <div className="container">
+        <TextField
+          name="searchText"
+          value={this.state.searchText}
+          onChange={this.onTextChange}
+          floatingLabelText="Search For Images"
+          fullWidth={true}
+        />
+        <br />
+        <SelectField
+          floatingLabelText="Amount"
+          value={this.state.amount}
+          onChange={this.onAmountChange}
+        >
+          <MenuItem value={5} primaryText="5" />
+          <MenuItem value={10} primaryText="10" />
+          <MenuItem value={15} primaryText="15" />
+          <MenuItem value={30} primaryText="30" />
+          <MenuItem value={50} primaryText="50" />
+        </SelectField>
+      </div>
+    );
+  }
+}
+
+export default Search;
