82d271188b42f15279fcfb8f93f9921217959b06
Rodgers M Gitau <rodgers.gitau@gmail.com>
2023-04-01 23:22:23 +0300

    chore: organise game assets & gameplay

--- noughts-and-crosses/src/index.js
+++ noughts-and-crosses/src/index.js
@@ -1,205 +1,319 @@
-// --------------------------------------
-// -------- Query Selectors -------------
-// --------------------------------------
-const canvas = document.querySelector("#board");
-const tracks = document.querySelectorAll("audio");
-const alertElement = document.querySelector("#alert");
-const btnSound = document.querySelector("#btn-sound");
+const WINNING_PROBABILITIES = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-// --------------------------------------
-// -------- LocalStorage ----------------
-// --------------------------------------
-function updateStorage() {
-  const supported = window.localStorage;
-  if (supported) {
-    const hasValue = window.localStorage.getItem("best-score");
-    if (hasValue) {
-      bestScore = localStorage.getItem("best-score");
+let currentPlayer = "X";
+let gameBoard = ["", "", "", "", "", "", "", "", ""];
+let avatarX, avatarO;
+
+const cells = document.querySelectorAll(".cell");
+
+const board = document.getElementById("board");
+
+const playerXFile = document.getElementById("player-x-file");
+const playerOFile = document.getElementById("player-o-file");
+
+const playerXAvatarAction = document.getElementById("player-x-avatar-action");
+const playerOAvatarAction = document.getElementById("player-o-avatar-action");
+
+const playerXAvatar = document.getElementById("player-x-avatar");
+const playerOAvatar = document.getElementById("player-o-avatar");
+
+const playerXName = document.getElementById("player-x-name");
+const playerOName = document.getElementById("player-o-name");
+
+const playerXScore = document.getElementById("player-x-score");
+const playerOScore = document.getElementById("player-o-score");
+
+const playerXTrophy = document.getElementById("player-x-trophy");
+const playerOTrophy = document.getElementById("player-o-trophy");
+
+const winline = document.getElementById("win-line");
+
+const resetGameAction = document.getElementById("reset-game-action");
+const resetScoreAction = document.getElementById("reset-score-action");
+const downloadAction = document.getElementById("download-action");
+const alrightAction = document.getElementById("alright-action");
+
+const confettiAnimation = document.getElementById("confetti");
+
+const drawModal = document.getElementById("draw-modal");
+
+function onMount() {
+  syncScoreStatus();
+
+  resetGameAction.addEventListener("click", onResetGame);
+  alrightAction.addEventListener("click", onResetGame);
+  resetScoreAction.addEventListener("click", onResetScore);
+  downloadAction.addEventListener("click", onDownload);
+
+  cells.forEach((cell) => cell.addEventListener("click", onCell));
+}
+
+function updateBoardAvatars(player, dataURL) {
+  const avatars = document.querySelectorAll(`.player-${player}`);
+
+  avatars.forEach((avatar) => (avatar.src = dataURL));
+}
+
+function onResetGame() {
+  gameBoard = ["", "", "", "", "", "", "", "", ""];
+  currentPlayer = "X";
+
+  cells.forEach((cell) => {
+    cell.textContent = "";
+    cell.classList.remove("disabled");
+  });
+
+  board.classList.remove("locked");
+
+  playerXName.classList.add("animate-bounce");
+  playerOName.classList.remove("animate-bounce");
+
+  drawModal.classList.add("hidden");
+
+  confettiAnimation.classList.remove("active");
+  playerXTrophy.classList.remove("active");
+  playerOTrophy.classList.remove("active");
+
+  winline.className = "";
+
+  resetGameAction.disabled = true;
+  downloadAction.disabled = true;
+}
+
+function onResetScore() {
+  localStorage.setItem("score-x", 0);
+  localStorage.setItem("score-o", 0);
+
+  syncScoreStatus();
+}
+
+function onCell(event) {
+  const { id } = event.target;
+
+  if (gameBoard[id] === "") {
+    updateBoard(id);
+  }
+
+  resetGameAction.disabled = false;
+}
+
+function updateBoard(index) {
+  gameBoard[index] = currentPlayer;
+
+  cells[index].innerHTML = getPlayerAvatar();
+
+  const gameStatus = checkGameStatus();
+
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+
+  if (!gameStatus) {
+    if (currentPlayer === "X") {
+      playerOName.classList.remove("animate-bounce");
+      playerXName.classList.add("animate-bounce");
     } else {
-      localStorage.setItem("best-score", bestScore);
-    }
-  }
-  if (score > bestScore) {
-    bestScore = score;
-    localStorage.setItem("best-score", bestScore);
-  }
-
-  if (score === maxScore) {
-    gameOver = true;
-    showAlert(`Maximum Score!! New best : ${score}`);
-  }
-  // scoreElement.innerHTML = score;
-  // bestScoreElement.innerHTML = bestScore;
-}
-
-// --------------------------------------
-// ------------- Game Logic -------------
-// --------------------------------------
-const ctx = canvas.getContext("2d");
-const VACANT = "#E0E9D1";
-const NOUGHT = "#f1be32";
-const CROSS = "#f472b6";
-const canvasSize = 500;
-const sectionSize = canvasSize / 3;
-canvas.width = canvasSize;
-canvas.height = canvasSize;
-ctx.translate(0.5, 0.5);
-
-let score = 0;
-let board = [];
-let player = 1;
-let audio = false;
-let bestScore = 0;
-let gameOver = false;
-const maxScore = 99999;
-
-// draw lines
-function drawLines(lineWidth, strokeStyle) {
-  const lineStart = 4;
-  const lineLenght = canvasSize - 5;
-  ctx.lineWidth = lineWidth;
-  ctx.lineCap = "round";
-  ctx.strokeStyle = strokeStyle;
-  ctx.beginPath();
-
-  // Horizontal lines
-  for (var y = 1; y <= 2; y++) {
-    ctx.moveTo(lineStart, y * sectionSize);
-    ctx.lineTo(lineLenght, y * sectionSize);
-  }
-
-  // Vertical lines
-  for (var x = 1; x <= 2; x++) {
-    ctx.moveTo(x * sectionSize, lineStart);
-    ctx.lineTo(x * sectionSize, lineLenght);
-  }
-
-  ctx.stroke();
-}
-
-//  draw the board
-function drawBoard() {
-  for (var x = 0; x < 3; x++) {
-    board.push([]);
-
-    for (var y = 0; y < 3; y++) {
-      board[x].push("");
-    }
-  }
-  return drawLines(10, VACANT);
-}
-
-// draw noughts
-function drawO(xCordinate, yCordinate) {
-  var halfSectionSize = 0.5 * sectionSize;
-  var centerX = xCordinate + halfSectionSize;
-  var centerY = yCordinate + halfSectionSize;
-  var radius = (sectionSize - 100) / 2;
-  var startAngle = 0 * Math.PI;
-  var endAngle = 2 * Math.PI;
-
-  ctx.lineWidth = 10;
-  ctx.strokeStyle = NOUGHT;
-  ctx.beginPath();
-  ctx.arc(centerX, centerY, radius, startAngle, endAngle);
-  ctx.stroke();
-}
-
-// draw crosses
-function drawX(xCordinate, yCordinate) {
-  ctx.strokeStyle = CROSS;
-
-  ctx.beginPath();
-
-  var offset = 50;
-  ctx.moveTo(xCordinate + offset, yCordinate + offset);
-  ctx.lineTo(
-    xCordinate + sectionSize - offset,
-    yCordinate + sectionSize - offset
-  );
-
-  ctx.moveTo(xCordinate + offset, yCordinate + sectionSize - offset);
-  ctx.lineTo(xCordinate + sectionSize - offset, yCordinate + offset);
-
-  ctx.stroke();
-}
-
-// add  piece
-function addPlayingPiece(mouse) {
-  let xCordinate;
-  let yCordinate;
-
-  for (var x = 0; x < 3; x++) {
-    for (var y = 0; y < 3; y++) {
-      xCordinate = x * sectionSize;
-      yCordinate = y * sectionSize;
-
-      if (
-        mouse.x >= xCordinate &&
-        mouse.x <= xCordinate + sectionSize &&
-        mouse.y >= yCordinate &&
-        mouse.y <= yCordinate + sectionSize
-      ) {
-        clearPlayingArea(xCordinate, yCordinate);
-
-        if (player === 1) {
-          drawX(xCordinate, yCordinate);
-        } else {
-          drawO(xCordinate, yCordinate);
-        }
-      }
+      playerXName.classList.remove("animate-bounce");
+      playerOName.classList.add("animate-bounce");
     }
   }
 }
 
-// get mouse position
-function getMousePosition(event) {
-  var rect = canvas.getBoundingClientRect();
+function checkGameStatus() {
+  let winClassName = null;
 
-  return {
-    x: event.clientX - rect.left,
-    y: event.clientY - rect.top,
-  };
+  for (let i = 0; i < WINNING_PROBABILITIES.length; i++) {
+    const [a, b, c] = WINNING_PROBABILITIES[i];
+
+    if (
+      gameBoard[a] &&
+      gameBoard[a] === gameBoard[b] &&
+      gameBoard[a] === gameBoard[c]
+    ) {
+      winClassName = `win-${a}${b}${c}`;
+      break;
+    }
+  }
+
+  if (winClassName !== null) {
+    announceWinner(winClassName);
+    return true;
+  }
+
+  if (!gameBoard.includes("")) {
+    drawModal.classList.remove("hidden");
+  }
+
+  return false;
 }
 
-// clear the board
-function clearPlayingArea(xCordinate, yCordinate) {
-  ctx.fillStyle = "transparent";
-  ctx.fillRect(xCordinate, yCordinate, sectionSize, sectionSize);
-}
+function announceWinner(winClassName) {
+  confettiAnimation.classList.add("active");
 
-canvas.addEventListener("mouseup", function (event) {
-  if (player === 1) {
-    player = 2;
+  board.classList.add("locked");
+
+  winline.classList.add("win");
+  winline.classList.add(winClassName);
+
+  playerXName.classList.remove("animate-bounce");
+  playerOName.classList.remove("animate-bounce");
+
+  if (currentPlayer === "X") {
+    playerXTrophy.classList.add("active");
   } else {
-    player = 1;
+    playerOTrophy.classList.add("active");
   }
-  let canvasMousePosition = getMousePosition(event);
-  addPlayingPiece(canvasMousePosition);
-});
 
-// start the game
-function play() {
-  hideAlert();
-  drawBoard();
+  updateScore();
+  fireConfetti();
+  syncScoreStatus();
 
-  if (!gameOver) {
-    hideAlert();
-    requestAnimationFrame(play);
+  downloadAction.disabled = false;
+
+  cells.forEach((cell, index) => {
+    if (!winClassName.includes(index)) {
+      cell.classList.add("disabled");
+    }
+  });
+}
+
+function updateScore() {
+  const key = `score-${currentPlayer.toLocaleLowerCase()}`;
+  const score = Number(localStorage.getItem(key) || 0) + 1;
+
+  localStorage.setItem(key, score);
+
+  if (currentPlayer === "X") {
+    playerXScore.textContent = score;
+  } else {
+    playerOScore.textContent = score;
   }
 }
 
-// reset the game
-function reset() {
-  return window.location.reload();
+function syncScoreStatus() {
+  const scoreX = Number(localStorage.getItem("score-x")) || 0;
+  const scoreO = Number(localStorage.getItem("score-o")) || 0;
+
+  playerXScore.textContent = scoreX;
+  playerOScore.textContent = scoreO;
+
+  resetScoreAction.disabled = !(scoreX > 0 || scoreO > 0);
+}
+
+function getPlayerAvatar() {
+  let avatar = avatarO;
+
+  if (currentPlayer === "X") {
+    avatar = avatarX;
+  }
+
+  return `<img class="h-14 w-14 md:h-28 md:w-28 rounded-full object-cover player-${currentPlayer.toLocaleLowerCase()}" src="${avatar}" alt="${currentPlayer}" />`;
+}
+
+function onDownload() {
+  const board = document.getElementById("canvas");
+
+  html2canvas(board, {
+    useCors: true,
+    ignoreElements: function (element) {
+      return element.classList.contains("ignore");
+    },
+  }).then(function (canvas) {
+    const newCanvas = document.createElement("canvas");
+    const ctx = newCanvas.getContext("2d");
+    newCanvas.width = canvas.width;
+    newCanvas.height = canvas.height + 120;
+    ctx.fillRect(0, 0, newCanvas.width, newCanvas.height);
+    ctx.drawImage(canvas, 20, 20);
+
+    const dataURL = newCanvas.toDataURL("image/png");
+
+    const link = document.createElement("a");
+    link.download = "Tic Tac Toe.png";
+    link.href = dataURL;
+
+    link.click();
+  });
+}
+
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign(
+      {},
+      {
+        disableForReduceMotion: true,
+      },
+      opts,
+      {
+        particleCount: Math.floor(200 * particleRatio),
+      }
+    )
+  );
+}
+
+function confettiExplosion(origin) {
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+    origin,
+  });
+
+  fire(0.2, {
+    spread: 60,
+    origin,
+  });
+
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    origin,
+  });
+
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    origin,
+  });
+
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+    origin,
+  });
+}
+
+function fireConfetti() {
+  const rect =
+    currentPlayer === "X"
+      ? playerXAvatar.getBoundingClientRect()
+      : playerOAvatar.getBoundingClientRect();
+
+  const center = {
+    x: rect.left + rect.width / 2,
+    y: rect.top + rect.height / 2,
+  };
+  const origin = {
+    x: center.x / window.innerWidth,
+    y: center.y / window.innerHeight,
+  };
+
+  confettiExplosion(origin);
 }
 
 // --------------------------------------
 // ------------- Audio ------------------
 // --------------------------------------
-function toggleAudio() {
+function toggleAudio(tracks) {
   if (!tracks) return;
-  btnSound.setAttribute("disabled", true);
+  // btnSound.setAttribute("disabled", true);
 
   try {
     tracks.forEach((track) => {
@@ -214,75 +328,28 @@ function toggleAudio() {
   } catch (error) {
     console.log(error);
   }
-  return btnSound.removeAttribute("disabled");
+  // return btnSound.removeAttribute("disabled");
 }
 
 function updateSoundElement() {
-  const soundOnIcon = document.querySelector("#sound-on");
-  const soundOffIcon = document.querySelector("#sound-off");
-
-  if (audio) {
-    audio = false;
-    soundOffIcon.classList.add("hidden");
-    soundOnIcon.classList.remove("hidden");
-    return;
-  }
-  audio = true;
-  soundOnIcon.classList.add("hidden");
-  soundOffIcon.classList.remove("hidden");
-  return;
-}
-
-// --------------------------------------
-// ------------- Controls ---------------
-// --------------------------------------
-btnSound.addEventListener("click", () => {
-  return updateSoundElement();
-});
-
-alertElement.querySelector("#alert-content").addEventListener("click", () => {
-  return play();
-});
-
-document.addEventListener("keydown", (event) => {
-  switch (`${event.code}`) {
-    case "KeyQ":
-      return toggleControls();
-    case "KeyP":
-      return play();
-    case "KeyR":
-      return reset();
-    case "KeyM":
-      return toggleAudio();
-    default:
-      return;
-  }
-});
-
-// --------------------------------------
-// -------- Alert Handler----------------
-// --------------------------------------
-function showAlert(alert) {
-  alertElement.classList.add("flex");
-  alertElement.classList.remove("hidden");
-  alertContent.innerHTML = alert;
-  setTimeout(() => {
-    alertElement.classList.remove("flex");
-    alertElement.classList.add("hidden");
-    reset();
-  }, 3000);
-}
-
-function hideAlert() {
-  alertElement.classList.add("hidden");
-  alertElement.classList.remove("flex");
+  // const soundOnIcon = document.querySelector("#sound-on");
+  // const soundOffIcon = document.querySelector("#sound-off");
+  // if (audio) {
+  //   audio = false;
+  //   soundOffIcon.classList.add("hidden");
+  //   soundOnIcon.classList.remove("hidden");
+  //   return;
+  // }
+  // audio = true;
+  // soundOnIcon.classList.add("hidden");
+  // soundOffIcon.classList.remove("hidden");
+  // return;
 }
 
 // --------------------------------------
 // -------- Window Loader ---------------
 // --------------------------------------
 window.addEventListener("load", () => {
-  updateStorage();
-  drawBoard();
-  // updateSoundElement();
+  onMount();
+  updateSoundElement();
 });
