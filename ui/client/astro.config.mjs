5c0c7e7d5ca46793103532ee53ac534b55188428
Rodgers Gitau <rodgersgitau@gmail.com>
2023-12-23 23:58:23 +0300

    feat: client app

--- ui/client/astro.config.mjs
+++ ui/client/astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    base : "/labs",
-    site: "https://rodgersgitau.github.io"
+  base: "/",
+  site: "https://rodgersgitau.github.io/labs",
+  integrations: [tailwind()]
 });
