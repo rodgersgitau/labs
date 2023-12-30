6b3df7431a4bdf24910963ff0f61e14deb9c7b90
Rodgers Gitau <rodgersgitau@gmail.com>
2023-12-31 02:22:14 +0300

    feat: games list

---
+++ apps/web/src/components/GameCard.tsx
@@ -0,0 +1,78 @@
+import { PlayCircleIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardFooter, CardHeader } from "@/components/ui/card";
+import { Icons } from "@/components/ui/icons";
+import Image from "@/components/ui/image";
+import Link from "@/components/ui/link";
+
+import { cn, slugify } from "@/lib/utils";
+import type { DataType } from "@/data";
+
+interface GameCardProps {
+  index: number;
+  item: DataType;
+  className?: string;
+}
+
+export default function GameCard({ item, className }: GameCardProps) {
+  return (
+    <Card
+      className={cn(
+        "relative mx-auto bg-card/40 w-full border-0 group hover:!scale-110 transition-all",
+        className
+      )}
+    >
+      <CardHeader>
+        <div className="flex items-center w-full h-full gap-4">
+          <div className="relative grid items-center w-48 p-2 transition-all group-hover:scale-110">
+            <Image
+              src={item.image}
+              alt={item.name}
+              className="object-cover w-full h-full mx-auto rounded !aspect-video"
+            />
+          </div>
+
+          <div className="grid flex-1 gap-4">
+            <p className="text-lg font-black uppercase">{item.name}</p>
+            <span className="border-b border-current/90"></span>
+            <p className="text-xs font-medium !leading-loose tracking-wide">
+              {item.description + "."}
+            </p>
+          </div>
+        </div>
+      </CardHeader>
+      <CardFooter className="grid items-center h-14">
+        <div className="flex items-center justify-start gap-4">
+          {item.link && (
+            <Button
+              asChild
+              variant="outline"
+              className="w-full border-current opacity-80 hover:border-0 hover:opacity-100 group-hover:bg-pink-800 group-hover:text-white group-hover:border-0 "
+            >
+              <Link href={`/games/${slugify(item.name)}`}>
+                <PlayCircleIcon className="w-5 h-5 mr-2" />
+                Play
+              </Link>
+            </Button>
+          )}
+          {item.repository && (
+            <Button asChild variant="default" className="w-full">
+              <Link target="_blank" href={item.repository}>
+                <Icons.gitHub className="w-5 h-5 mr-2" />
+                Repo
+              </Link>
+            </Button>
+          )}
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
+
+//   name: string;
+//   type: "game" | "effect";
+//   link?: string;
+//   image: string;
+//   repository: string;
+//   description: string;
--- apps/web/src/components/MainNav.tsx
+++ apps/web/src/components/MainNav.tsx
@@ -1,7 +1,6 @@
-import { Image } from '@/components/ui/image';
-import Link from '@/components/ui/link';
-import { siteConfig } from '@/config';
-import { cn } from '@/lib/utils';
+import Link from "@/components/ui/link";
+import { siteConfig } from "@/config";
+import { cn } from "@/lib/utils";
 
 interface Props {
   pathname: string;
@@ -10,28 +9,6 @@ interface Props {
 export default function MainNav({ pathname }: Props) {
   return (
     <div className="relative z-50 items-center hidden w-full gap-10 py-4 md:flex">
-      <Link
-        href="/"
-        className="relative flex items-center w-24 dark:hidden h-14"
-      >
-        <Image
-          ratio={96 / 56}
-          src="/images/logo.svg"
-          className="object-cover w-full h-full"
-        />
-      </Link>
-
-      <Link
-        href="/"
-        className="relative items-center hidden w-24 dark:flex h-14"
-      >
-        <Image
-          ratio={96 / 56}
-          src="/images/logo-dark.svg"
-          className="object-cover w-full h-full"
-        />
-      </Link>
-
       <nav className="flex items-center justify-center flex-1 gap-10">
         {siteConfig.navLinks.map((link) => (
           <Link
